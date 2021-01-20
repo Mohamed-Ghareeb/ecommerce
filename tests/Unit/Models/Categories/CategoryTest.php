41a723600a43891703fe014b6ce533ef27d8b354
Mohamed-Ghareeb <6624881123za@gmail.com>
2021-01-20 04:02:01 +0200

    Adding Variation Type For The ProductVariation

--- tests/Unit/Models/Categories/CategoryTest.php
+++ tests/Unit/Models/Categories/CategoryTest.php
@@ -5,10 +5,7 @@ namespace Tests\Unit\Models\Categories;
 use Tests\TestCase;
 use App\Models\Category;
 use App\Models\Product;
-use App\Models\RelationTraits\RelationOfCategory;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CategoryTest extends TestCase
 {
